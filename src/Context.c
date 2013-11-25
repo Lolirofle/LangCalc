63e2f3c0826e26ca83100ab393b2d7070e089d74
Lolirofle <lolipopple@hotmail.com>
2013-11-25 20:14:36 +0100

    Memory equals renaming

--- src/Context.c
+++ src/Context.c
@@ -6,7 +6,7 @@
 
 const struct Type* Context_getType(const struct Context* context,Stringcp name){
 	LinkedList_forEach(context->types,node){
-		if(name.length==((struct Type*)node->ptr)->name.length && Data_equals(((struct Type*)node->ptr)->name.ptr,name.ptr,name.length))
+		if(name.length==((struct Type*)node->ptr)->name.length && Memory_equals(((struct Type*)node->ptr)->name.ptr,name.ptr,name.length))
 			return (struct Type*)node->ptr;
 	}
 	return NULL;
@@ -14,7 +14,7 @@ const struct Type* Context_getType(const struct Context* context,Stringcp name){
 
 const struct Function* Context_getFunction(const struct Context* context,Stringcp name){
 	LinkedList_forEach(context->functions,node){
-		if(name.length==((struct Function*)node->ptr)->name.length && Data_equals(((struct Function*)node->ptr)->name.ptr,name.ptr,name.length))
+		if(name.length==((struct Function*)node->ptr)->name.length && Memory_equals(((struct Function*)node->ptr)->name.ptr,name.ptr,name.length))
 			return (struct Function*)node->ptr;
 	}
 	return NULL;
@@ -22,7 +22,7 @@ const struct Function* Context_getFunction(const struct Context* context,Stringc
 
 const struct Variable* Context_getVariable(const struct Context* context,Stringcp name){
 	LinkedList_forEach(context->variables,node){
-		if(name.length==((struct Variable*)node->ptr)->name.length && Data_equals(((struct Variable*)node->ptr)->name.ptr,name.ptr,name.length))
+		if(name.length==((struct Variable*)node->ptr)->name.length && Memory_equals(((struct Variable*)node->ptr)->name.ptr,name.ptr,name.length))
 			return (struct Variable*)node->ptr;
 	}
 	return NULL;
